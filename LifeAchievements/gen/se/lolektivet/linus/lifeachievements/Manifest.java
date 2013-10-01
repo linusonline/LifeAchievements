2693bbb476625eb9b93bcee7b1eaca4c2ef8ddad
Linus Lindholm <linusonline@gmail.com>
2013-10-01 22:20:13 +0200

    Remove generated and output files from repo.

--- LifeAchievements/gen/se/lolektivet/linus/lifeachievements/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package se.lolektivet.linus.lifeachievements;
-
-/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
-public final class Manifest {
-}
